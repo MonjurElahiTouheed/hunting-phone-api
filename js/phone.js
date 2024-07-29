9792de124618717781302396bb539f45cad95f37
MonjurElahiTouheed <monjurelahitouheed@gmail.com>
2024-07-29 14:34:28 +0600

    created show all button

---
+++ js/phone.js
@@ -0,0 +1,86 @@
+const loadPhone = async (searchText, isShowAll) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+    const data = await res.json();
+    const phones = data.data;
+    // console.log(phones);
+    displayPhones(phones, isShowAll)
+}
+
+const displayPhones = (phones, isShowAll) => {
+    const phoneContainer = document.getElementById('phone-container');
+    // clear phone container cards before adding new cards
+    phoneContainer.textContent = '';
+    const showAllContainer = document.getElementById('show-all-container');
+    if(phones.length > 12 && !isShowAll){
+      showAllContainer.classList.remove('hidden');
+    }
+    else{
+      showAllContainer.classList.add('hidden');
+    }
+    console.log('is ShowAll: ',isShowAll);
+    // display only first 12 phones if not show All
+    if(!isShowAll){
+      phones = phones.slice(0,12);
+    }
+
+    phones.forEach(phone => {
+        console.log(phone);
+        // 2 create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-base-100 p-4 shadow-xl`;
+        // 3 set  innerHTML
+        phoneCard.innerHTML = `
+        <figure>
+                      <img
+                        src="${phone.image}"
+                        alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                      <h2 class="card-title">${phone.phone_name}</h2>
+                      <p>If a dog chews shoes whose shoes does he choose?</p>
+                      <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                      </div>
+                    </div>
+        `;
+        // 4 append child
+        phoneContainer.appendChild(phoneCard);
+
+    })
+
+    // hide loading spinner
+    toggleLoadingSpinner(false);
+}
+
+// handle search button 
+const handleSearch = (isShowAll) => {
+  toggleLoadingSpinner(true);
+  const searchField = document.getElementById('search-field');
+  const searchText = searchField.value;
+  console.log(searchText);
+  loadPhone(searchText, isShowAll);
+}
+
+/* const handleSearch2 = () => {
+  toggleLoadingSpinner(true);
+  const searchField = document.getElementById('search-field2');
+  const searchText  = searchField.value;
+  loadPhone(searchText);
+} */
+
+const toggleLoadingSpinner = (isLoading) => {
+  const loadingSpinner = document.getElementById('loading-spinner');
+  if(isLoading){
+    loadingSpinner.classList.remove('hidden');
+  }
+  else{
+    loadingSpinner.classList.add('hidden');
+  }
+}
+
+// handle show all
+const handleShowAll = () => {
+  handleSearch(true)
+}
+
+loadPhone();
