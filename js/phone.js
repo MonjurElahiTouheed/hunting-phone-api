235aba95485d4fbff2d1e702b0d9fb35658ecfae
MonjurElahiTouheed <monjurelahitouheed@gmail.com>
2024-07-30 08:07:54 +0600

    created modal for show details button

--- js/phone.js
+++ js/phone.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText, isShowAll) => {
+const loadPhone = async (searchText=13, isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
@@ -17,14 +17,15 @@ const displayPhones = (phones, isShowAll) => {
     else{
       showAllContainer.classList.add('hidden');
     }
-    console.log('is ShowAll: ',isShowAll);
+    // console.log('is ShowAll: ',isShowAll);
     // display only first 12 phones if not show All
     if(!isShowAll){
       phones = phones.slice(0,12);
     }
 
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone.slug);
+        // console.log(phone);
         // 2 create a div
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card bg-base-100 p-4 shadow-xl`;
@@ -38,8 +39,8 @@ const displayPhones = (phones, isShowAll) => {
                     <div class="card-body">
                       <h2 class="card-title">${phone.phone_name}</h2>
                       <p>If a dog chews shoes whose shoes does he choose?</p>
-                      <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                      <div class="card-actions justify-center">
+                        <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
                       </div>
                     </div>
         `;
@@ -52,14 +53,54 @@ const displayPhones = (phones, isShowAll) => {
     toggleLoadingSpinner(false);
 }
 
+// 
+const handleShowDetail = async(id) => {
+  // console.log('id ', id);
+  // load single phone data
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+  const data = await res.json();
+  const phone = data.data;
+  showPhoneDetails(phone);
+}
+
+
+const showPhoneDetails = (phone) => {
+  console.log(phone);
+  const phoneName = document.getElementById('show-detail-phone-name');
+  phoneName.innerText = `${phone.name}`;
+  const showDetailContainer = document.getElementById('show-detail-container');
+  showDetailContainer.innerHTML = `
+    <img src="${phone.image}" alt= ""/>
+    <p><span>Storage:</span>${phone?.mainFeatures?.storage}</p>
+    <p><span>Display Size:</span>${phone?.mainFeatures?.displaySize}</p>
+    <p><span>Chipset:</span>${phone?.mainFeatures?.chipSet}</p>
+    <p><span>Memory:</span>${phone?.mainFeatures?.memory}</p>
+    <p><span>Slug:</span>${phone?.slug}</p>
+    <p><span>Release data:</span>${phone?.releaseDate}</p>
+    <p><span>Brand:</span>${phone?.
+      brand}</p>
+    <p><span>GPS:</span>${phone?.others?.GPS}</p>
+  `;
+  // show the modal
+  show_details_modal.showModal();
+}
+
+
 // handle search button 
-const handleSearch = (isShowAll) => {
+const handleSearch = (event, isShowAll) => {
   toggleLoadingSpinner(true);
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value;
-  console.log(searchText);
+  // console.log(searchText);
   loadPhone(searchText, isShowAll);
+  
 }
+
+document.getElementById('search-field').addEventListener('keyup', (event) => {
+  if(event.key === 'Enter'){
+    handleSearch();
+  }
+})
 
 /* const handleSearch2 = () => {
   toggleLoadingSpinner(true);
